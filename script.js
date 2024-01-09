e375795e5334c7b5a0981197547f630a017343c0
sakshi543 <saaksshi23@gmail.com>
2024-01-09 16:15:54 +0530

    Made template

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // You can add code here to handle the form submission (e.g., using Fetch API)
+    alert('Form submitted!');
+});
